d7e3a4e02244df0ec9ed3a52fa080b130882ef2c
Aleksandra Jaroszewska <aleksandrajaroszewska@MacBook-Pro-ng-local-4.local>
2023-09-10 08:19:59 +0200

    (mod-1): naming and structure correction for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { ProductList } from "@/components/organisms/ProductList";
-
 export default function Home() {
 	return (
 		<main className=" bg-pink-800">
-			<title className="pt-4 text-center text-xl text-gray-100">
-				base product list
-			</title>
-			<div>
-				<ProductList />
-			</div>
+			<h1>Store</h1>
 		</main>
 	);
 }
