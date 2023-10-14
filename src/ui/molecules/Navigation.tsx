0765ecdc8b2ea5ae223c97ab9e1e91c15d2320d2
Aleksandra Jaroszewska <aleksandra.jaroszewska13@gmail.com>
2023-10-14 08:41:47 +0200

    correction

--- src/ui/molecules/Navigation.tsx
+++ src/ui/molecules/Navigation.tsx
@@ -21,7 +21,7 @@ export async function Navigation() {
 	const modalUrl = "/cart" as Route<"string">;
 
 	return (
-		<nav className=" sticky top-0 z-10 opacity-70" role="navigation">
+		<nav className="sticky top-0 z-10 opacity-70" role="navigation">
 			<ul className=" flex items-center justify-between bg-fuchsia-900 px-24 py-4 text-fuchsia-100 ">
 				<li className="text-fuchsia-100">
 					<ActiveLink exact={true} href="/">
@@ -36,29 +36,13 @@ export async function Navigation() {
 				<li className="text-fuchsia-100">
 					<ActiveLink href="/categories">categories</ActiveLink>
 				</li>
-
-				<li className="text-fuchsia-100">
-					<ActiveLink exact={false} href="/categories/t-shirts/1">
-						t-shirts
-					</ActiveLink>
-				</li>
-				<li className="text-fuchsia-100">
-					<ActiveLink exact={false} href="/categories/hoodies/1">
-						hoodies
-					</ActiveLink>
-				</li>
-				<li className="text-fuchsia-100">
-					<ActiveLink exact={false} href="/categories/accessories/1">
-						accesories
-					</ActiveLink>
-				</li>
-				<li className="flex">
-					<ShoppingCart color="white" size={20} />
-					<ActiveLink className="ml-2" href={modalUrl}>
-						Cart ({quantity})
-					</ActiveLink>
-				</li>
 			</ul>
+			<div className="flex">
+				<ShoppingCart color="white" size={20} />
+				<ActiveLink className="ml-2" href={modalUrl}>
+					Cart ({quantity})
+				</ActiveLink>
+			</div>
 		</nav>
 	);
 }
